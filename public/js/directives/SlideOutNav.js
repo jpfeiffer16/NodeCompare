9eda03db38064abe22d77ed11767490f22c8793c
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-09-16 17:08:59 -0600

    Working on UI

--- public/js/directives/SlideOutNav.js
+++ public/js/directives/SlideOutNav.js
@@ -1,10 +1,46 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 angular.module('app')
   .directive('slideOutNav', function() {
+    
+    
+    function showNav() {
+      var slideOutNav = $('.slide-out-nav');
+      var cover = $('#cover');
+      slideOutNav.addClass('active');
+      
+      // cover.show();
+      cover.fadeIn();
+    }
+    
+    
+    function hideNav() {
+      var slideOutNav = $('.slide-out-nav');
+      var cover = $('#cover');
+      // cover.hide();
+      cover.fadeOut();
+      slideOutNav.removeClass('active');
+    }
+    
+    
+    
     return {
       link: function(scope, element, attrs) {
         element.on('click', function(e) {
-          $('.slide-out-nav').addClass('active');
+          var slideOutNav = $('.slide-out-nav');
+          var cover = $('#cover');
+          
+          showNav();
+          e.stopPropagation();
+          
+          
+          cover.on('click', function(e) {
+            hideNav();
+            cover.off('click');
+          });
+          slideOutNav.find('a').on('click', function(e) {
+            hideNav();
+            slideOutNav.find('a').off('click');
+          });
         });
       }
     }
