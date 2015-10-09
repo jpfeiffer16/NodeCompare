5302f5cd602efede801c890fb152cb5ebb8032ed
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-10-08 19:19:31 -0600

    cleanup

--- app/controllers/imageprovider.js
+++ app/controllers/imageprovider.js
@@ -13,7 +13,6 @@ router.get('/imageprovider/:id', function(req, res, next) {
   Image.findOne({ _id: id }, function(err, result) {
     if (!err && result != null) {
       var image = new Buffer(result.data, 'base64');
-      
       res.send(image);
     } else {
       res.status(404).send();
